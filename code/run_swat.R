33c39b4d5872bba14d24e1c85b073b957fbe1bcb
bryanparthum <parthum.bryan@epa.gov>
2022-12-01 02:18:16 +0000

    populate project

---
+++ code/run_swat.R
@@ -0,0 +1,22 @@
+## Written by: US EPA, National Center for Environmental Economics; December 2022
+
+##########################
+#################  library
+##########################
+
+## Clear worksace
+rm(list = ls())
+gc()
+
+## This function will check if a package is installed, and if not, install it
+list.of.packages <- c('magrittr','tidyverse')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
+lapply(list.of.packages, library, character.only = TRUE)
+
+##########################
+#################### parts
+##########################
+
+## run swat
+system('inputs/swat670')
